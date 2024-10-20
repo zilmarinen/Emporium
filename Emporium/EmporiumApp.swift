aae30768728c956e6f663efdbfea88e6f9d0f249
Zack Brown <zack@zackbrown.co.uk>
2024-10-20 11:28:09 +0100

    Initial commit

---
+++ Emporium/EmporiumApp.swift
@@ -0,0 +1,19 @@
+//
+//  EmporiumApp.swift
+//
+//  Created by Zack Brown on 20/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct EmporiumApp: App {
+    
+    var body: some Scene {
+        
+        WindowGroup {
+            
+            ContentView()
+        }
+    }
+}
